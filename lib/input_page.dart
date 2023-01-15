4eac9ee36eff3b3b4a46094ebc901d0d90d01386
nativacu <nativacu@gmail.com>
2023-01-15 02:29:23 -0400

    feat: adding slider to input height

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,10 @@
+import 'package:bmi_calculator_flutter/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'custom_card.dart';
 import 'gender_options.dart';
 
 const double buttonHeight = 70.0;
-const Color buttonColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -14,6 +14,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  //TODO extract Slider to new widget
+  int bmiHeight = 180;
+
   @override
   Widget build(BuildContext context) {
     final h = MediaQuery.of(context).size.height;
@@ -23,14 +26,53 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           // Row(
           //   children: [
-          Expanded(
+          const Expanded(
             child: GenderOptions(),
           ),
           Expanded(
-            child: CustomCard(),
+            //TODO extract Slider to new widget
+            child: CustomCard(
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Height',
+                    style: kSecondaryTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        bmiHeight.toString(),
+                        style: kPrimaryTextStyle,
+                      ),
+                      const Text(
+                        'cm',
+                        style: kSecondaryTextStyle,
+                      ),
+                    ],
+                  ),
+                  Slider(
+                    value: bmiHeight.toDouble(),
+                    min: 120,
+                    max: 220,
+                    activeColor: kAccentColor,
+                    inactiveColor: kSecondaryContentColor,
+                    onChanged: (double test) {
+                      setState(() {
+                        bmiHeight = test.round();
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
           ),
           Expanded(
             child: Row(
@@ -45,7 +87,7 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
           Container(
-            color: buttonColor,
+            color: kAccentColor,
             margin: const EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: h * 0.09,
